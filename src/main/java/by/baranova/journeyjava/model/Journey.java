86db74b301bd113980ff101a0ef1d84efcbc2403
BaranovaKate <katte.bar19@gmail.com>
2024-04-24 22:04:10 +0300

    fix code smells

--- src/main/java/by/baranova/journeyjava/model/Journey.java
+++ src/main/java/by/baranova/journeyjava/model/Journey.java
@@ -2,9 +2,15 @@ package by.baranova.journeyjava.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+/**
+ * Entity class representing a journey.
+ */
+
+@Data
 @Entity
 @Table(name = "journeys")
 public class Journey {
@@ -38,71 +44,4 @@ public class Journey {
     @JoinColumn(name = "travel_agency_id", nullable = false)
     @JsonIgnoreProperties("journeys")
     private TravelAgency travelAgency;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
-    public void setTown(String town) {
-        this.town = town;
-    }
-
-    public LocalDate getDateToJourney() {
-        return dateToJourney;
-    }
-
-    public void setDateToJourney(LocalDate dateToJourney) {
-        this.dateToJourney = dateToJourney;
-    }
-
-    public LocalDate getDateFromJourney() {
-        return dateFromJourney;
-    }
-
-    public void setDateFromJourney(LocalDate dateFromJourney) {
-        this.dateFromJourney = dateFromJourney;
-    }
-
-    public TravelAgency getTravelAgency() {
-        return travelAgency;
-    }
-
-    public void setTravelAgency(TravelAgency travelAgency) {
-        this.travelAgency = travelAgency;
-    }
-
-    @Override
-    public String toString() {
-        return "Journey{" +
-                "id=" + id +
-                ", country='" + country + '\'' +
-                ", town='" + town + '\'' +
-                ", dateToJourney=" + dateToJourney +
-                ", dateFromJourney=" + dateFromJourney +
-                ", travelAgency=" + travelAgency +
-                '}';
-    }
-
-
-    /** empty constructor. */
-    public Journey() {
-    }
-
 }
--- src/main/java/by/baranova/journeyjava/model/TravelAgency.java
+++ src/main/java/by/baranova/journeyjava/model/TravelAgency.java
@@ -2,9 +2,11 @@ package by.baranova.journeyjava.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 @Entity
 @Table(name = "travel_agencies")
 public class TravelAgency {
@@ -22,41 +24,4 @@ public class TravelAgency {
             cascade = CascadeType.ALL)
     @JsonIgnoreProperties("travelAgency")
     private List<Journey> journeys;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Journey> getJourneys() {
-        return journeys;
-    }
-
-    public void setJourneys(List<Journey> journeys) {
-        this.journeys = journeys;
-    }
-
-    @Override
-    public String toString() {
-        return "TravelAgency{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", journeys=" + journeys +
-                '}';
-    }
-
-    /** empty constructor. */
-    public TravelAgency() {
-    }
 }
